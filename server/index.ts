d416441feb0eaca722d1fd6ddf77306c4a34a834
EUBI-BioHub <eubi-biohub@mac-ellenberg48.localdomain>
2025-12-24 17:37:45 +0100

    Fix: Bind to 0.0.0.0 in production for Cloud Run

--- server/index.ts
+++ server/index.ts
@@ -74,10 +74,11 @@ app.use(express.urlencoded({ extended: false }));
   }
 
   const port = parseInt(process.env.PORT || "5000", 10);
+  const host = process.env.NODE_ENV === 'production' ? '0.0.0.0' : '127.0.0.1';
   httpServer.listen(
     {
       port,
-      host: "127.0.0.1",
+      host,
     },
     () => {
       log(`serving on port ${port}`);
